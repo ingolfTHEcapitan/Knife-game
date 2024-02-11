a922bea49a99486d426bc22da5ccba9cda471719
Ingolf <vitaly.litov4encko2017@gmail.com>
2024-02-12 00:03:53 +1000

    Тест кодировки гитхаба

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,35 +3,35 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("Audio Sourse")]
-    [SerializeField] AudioSource musicSourse;   // Источник звука для фоновой музыки.
-    [SerializeField] AudioSource SFXSourse;     // Источник звука для звуковых эффектов.
+    [SerializeField] AudioSource musicSourse;   // Источник звука для фоновой музыки
+    [SerializeField] AudioSource SFXSourse;     // Источник звука для звуковых эффектов
 
     [Header("Audio clip")]
-    public AudioClip background; // Звук фоновой музыки.
-    public AudioClip attack;     // Звук атаки.
-    public AudioClip miss;       // Звук промаха.
+    public AudioClip background; // Звук фоновой музыки
+    public AudioClip attack;     // Звук атаки
+    public AudioClip miss;       // Звук промаха
 
     private void Start()
     {
-        // Устанавливаем фоновую музыку и начинаем её воспроизведение при старте скрипта.
+        // Устанавливаем фоновую музыку и начинаем её воспроизведение при старте скрипта
         musicSourse.clip = background;
         musicSourse.Play();
     }
 
-    // Метод для воспроизведения звуковых эффектов.
+    // Метод для воспроизведения звуковых эффектов
     public void PlaySFX(AudioClip clip)
     {
         SFXSourse.PlayOneShot(clip);
         
     }
 
-    // Метод для приостановки фоновой музыки.
+    // Метод для приостановки фоновой музыки
     public void PauseMusic()
     {
         musicSourse.Pause();
     }
 
-    // Метод для возобновления воспроизведения фоновой музыки после паузы.
+    // Метод для возобновления воспроизведения фоновой музыки после паузы
     public void UnPauseMusic()
     {
         musicSourse.UnPause();
