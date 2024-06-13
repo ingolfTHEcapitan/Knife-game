250721e4ddf1fc9b8c932f61c00ce94a1936ff30
Ingolf <vitaly.litov4encko2017@gmail.com>
2024-06-13 12:25:50 +1000

    Упразднил в классе Target функции Spawn1 и Spawn2 до Spawn

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(2f, 1.5f));
-            Spawn1(); 
+            Spawn(shield1Prefab, 3f, 6f, 1.5f, 3f); 
         }
     }
 
@@ -28,21 +28,14 @@ public class Spawner : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(6f, 4f));
-            Spawn2();
+            Spawn(shield2Prefab, 2f, 4f, 3.5f, 5f);
         }
     }
 
-    private void Spawn1()
+    private void Spawn(Target prefab, float speedMin, float speedmax, float sizeMin, float sizeMax)
     {
-        Target shield1 = Instantiate(shield1Prefab);
-        shield1.SetSpeed(3f, 6f);
-        shield1.SetSize(1.5f, 3f);
-    }
-
-    private void Spawn2()
-    {
-        Target shield2 = Instantiate(shield2Prefab);
-        shield2.SetSpeed(2f, 4f);
-        shield2.SetSize(3.5f, 5f);
+        Target shield = Instantiate(prefab);
+        shield.SetSpeed(speedMin, speedmax);
+        shield.SetSize(sizeMin, sizeMax);
     }
 }
