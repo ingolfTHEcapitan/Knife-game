668e90d298f1463fc7d24af72f5620b7a734b9aa
Ingolf <vitaly.litov4encko2017@gmail.com>
2024-09-27 13:51:25 +1000

    style(scorring): изменён кодстайл переменных в классе ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,40 +3,40 @@ using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI highScoreText;
+	[SerializeField] private TextMeshProUGUI _scoreText;
+	[SerializeField] private TextMeshProUGUI _highScoreText;
 
-    private int scoreNumber;
+	private int _scoreNumber;
 
-    private void Start()
-    {
-        // Устанавливаем текст для поля High Score из сохраненных данных.
-        highScoreText.SetText($"High Score: {PlayerPrefs.GetInt("highScore", 0)}");
-    }
+	private void Start()
+	{
+		// Устанавливаем текст для поля High Score из сохраненных данных.
+		_highScoreText.SetText($"High Score: {PlayerPrefs.GetInt("highScore", 0)}");
+	}
 
-    void OnEnable() => GlobalEventManager.EnemyKilled += IncreaseScore;
-    void OnDisable() => GlobalEventManager.EnemyKilled -= IncreaseScore;
+	void OnEnable() => GlobalEventManager.EnemyKilled += IncreaseScore;
+	void OnDestroy() => GlobalEventManager.EnemyKilled -= IncreaseScore;
 
-    public void IncreaseScore(int value)
-    {
-        scoreNumber += value;
+	public void IncreaseScore(int value)
+	{
+		_scoreNumber += value;
 
-        // Обновляем текст для поля текущего счета.
-        scoreText.SetText($"Score: {scoreNumber}");
+		// Обновляем текст для поля текущего счета.
+		_scoreText.SetText($"Score: {_scoreNumber}");
 
-        // Проверяем, установлен ли новый рекорд, и обновляем его при необходимости.
-        UpdateHighScore();
-    }
+		// Проверяем, установлен ли новый рекорд, и обновляем его при необходимости.
+		UpdateHighScore();
+	}
 
-    private void UpdateHighScore()
-    {
-        // Проверяем, превышает ли текущий счет лучший результат.
-        if (scoreNumber > PlayerPrefs.GetInt("highScore", 0))
-        {
-            // Если да, обновляем лучший результат в сохраненных данных.
-            PlayerPrefs.SetInt("highScore", scoreNumber);
-            // Обновляем текст для поля "Лучший результат" на экране.
-            highScoreText.SetText($"High Score: {scoreNumber}");
-        }
-    }
+	private void UpdateHighScore()
+	{
+		// Проверяем, превышает ли текущий счет лучший результат.
+		if (_scoreNumber > PlayerPrefs.GetInt("highScore", 0))
+		{
+			// Если да, обновляем лучший результат в сохраненных данных.
+			PlayerPrefs.SetInt("highScore", _scoreNumber);
+			// Обновляем текст для поля "Лучший результат" на экране.
+			_highScoreText.SetText($"High Score: {_scoreNumber}");
+		}
+	}
 }
