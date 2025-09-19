115747875f8d67e2cd9f7c599ae22bc2a9110f17
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2025-09-19 19:56:16 +1000

    feat(ui): сокрытие кнопки выхода на WebGl платформе

--- Assets/Project/Scripts/MainMenu.cs
+++ Assets/Project/Scripts/MainMenu.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Project.Scripts
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField] private Button _exitButton;
+        
+        private void Awake()
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+                _exitButton.gameObject.SetActive(false);
+        }
+
         public void PlayGame() => SceneManager.LoadSceneAsync("Game");
 
         public void Reset() => PlayerPrefs.DeleteKey("highScore");
